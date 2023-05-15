bc810f02050af291c486bd3844f46872a4c28061
Neutral Face <106446087+NeutralFace177@users.noreply.github.com>
2023-05-15 10:19:31 -0700

    Update script.js

--- dino/script.js
+++ dino/script.js
@@ -13,6 +13,23 @@ let alive = true;
 var smallY = 504;
 var bigY = 482;
 
+var fps = document.getElementById("fps");
+var startTime = Date.now();
+var frame = 0;
+
+function tick() {
+  var time = Date.now();
+  frame++;
+  if (time - startTime > 1000) {
+      fps.innerHTML = (frame / ((time - startTime) / 1000)).toFixed(1);
+      startTime = time;
+      frame = 0;
+	}
+  window.requestAnimationFrame(tick);
+}
+tick();
+
+
 class Cloud {
     constructor(x, y) {
 
